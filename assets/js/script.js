7ca68203e0f204114ce72caaddf1c0362c13fe0e
Clinto07 <kevinclinton.cars@gmail.com>
2022-09-05 22:50:04 +0000

    Debugging js and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,30 @@
-var userChoice;
-var computerChoice;
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages =  document.getElementById("messages");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-function compare(comChoice) {
-	if (userChoice === comChoice) {
-    return "The computer also chose " + computerChoice + ":" + "<br>" + "the result is a tie! :\|";
-	} else if (userChoice === "rock") {
-		if (comChoice === "paper") {
-      return "paper covers rock <br> You Lose :\(";
-		} else if (comChoice === "scissors") {
-      return "rock crushes scissors <br> You Win! :\)";
-		} else if (comChoice === "lizard") {
-			return "rock crushes lizard <br> You Win! :\) ";
-		} else {
-			return "spock vaporizes rock <br> You Lose :\(";
-		}
-	} else if (userChoice === "paper") {
-		if (comChoice === "rock") {
-			return "paper covers rock <br> You Win! :\)";
-		} else if (comChoice === "scissors") {
-			return "scissors cut paper <br> You Lose :\(";
-		} else if (comChoice === "lizard") {
-			return "lizard eats paper <br> You Lose :\(";
-		} else {
-			return "paper disproves spock <br> You Win! :\)";
-		}
-	}else if (userChoice === "scissors") {
-		if (comChoice === "rock") {
-			return "rock crushes scissors <br> You Lose :\(";
-		} else if (comChoice === "paper") {
-			return "scissors cut paper <br> You Win! :\)";
-		} else if (comChoice === "lizard") {
-			return "scissors decapitate lizard <br> You Win! :\)";
-		} else {
-			return "spock smashes scissors <br> You Lose :\(";
-		}
-	} else if (userChoice === "lizard") {
-		if (comChoice === "rock") {
-			return "rock crushes lizard <br> You Lose :\(";
-		} else if (comChoice === "paper") {
-			return "lizard eats paper <br> You Win! :\)";
-		} else if (comChoice === "scissors") {
-			return "scissors decapitate lizard <br> You Lose :\(";
-		} else {
-			return "lizard poisons spock <br> You Win! :\)";
-		}
-	} else if (userChoice === "spock") {
-		if (comChoice === "rock") {
-			return "spock vaporizes rock <br> You Win! :\)";
-		} else if (comChoice === "paper") {
-			return "paper disproves spock <br> You Lose :\(";
-		} else if (comChoice === "scissors") {
-			return "spock smashes scissors <br> You Win! :\) ";
-		} else {
-			return "lizard poisons spock <br> You Lose :\(";
-		}
-	}
-};
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playersChoice);
+    });
+}
 
-$(document).ready(function(){
-	$("button").click(function(){
-		userChoice = this.id;
-		computerChoice = Math.floor(Math.random() * 5);
-     switch (computerChoice) { 
-       case 0: computerChoice = "rock"; 
-         break; 
-       case 1: computerChoice = "paper"; 
-         break; 
-       case 2: computerChoice = "scissors"; 
-         break; 
-       case 3: computerChoice = "lizard";
-         break;
-       case 4: computerChoice = "spock"; 
-         break; 
-     }
+function playGame(playersChoice) {
 
+playerImage.src = `assets/images/${choices[playerChoice]}.jpg`;
+playerImage.alt = choices[playerChoice];
 
-		var result = compare(computerChoice).toUpperCase();
+let computerChoice = Math.floor(Math.random() *5);
 
-		$(".result").html("<h1>Result:</h1><p>User: " + userChoice.toUpperCase() + "<br>" +
-											"Computer: " + computerChoice.toUpperCase() + "</p>" + "<p>" + result + "</p>");
-	});
-});   
+computerImage.src = `assets/images/${choices[computerChoice]}.jpg`;
+computerImage.alt = choices[computerImage];
+
+let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+}
+
+function checkWinner()
+    
