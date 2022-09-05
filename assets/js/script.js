8d5d3f6df4e635590f17f9c8712392263ee7f68d
Clinto07 <kevinclinton.cars@gmail.com>
2022-09-05 20:10:43 +0000

    Add js coding for player and computer choice

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages =  document.getElementById("messages");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playersChoice);
+    });
+}
+
+function playGame(playersChoice) {
+
+playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+playerImage.alt = choices[playerChoice];
+
+let computerChoice = Math.floor(Math.random() *5);
+
+computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+computerImage.alt = choices[computerImage];
+
+let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+updateScores(result);
+
+}
